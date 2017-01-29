f66b557c1e30005dd4e802f9f8459aa5642d2ce8
Taye Adeyemi <dev@taye.me>
2017-01-29 13:41:43 +0100

    recompile

--- dist/domator.js
+++ dist/domator.js
@@ -10,15 +10,128 @@ var deepmerge = _interopDefault(require('deepmerge'));
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
   return typeof obj;
 } : function (obj) {
-  return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj;
+  return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
 };
 
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
+
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
+
 var regexes = {
-  tag: /^([a-z0-9\-]+)/,
-  id: /^#([a-z0-9\-]+)/,
-  className: /^\.([a-z0-9\-]+)/,
-  attr: /^\[([a-z\-0-9]+)(?:="([^"]+)")?\]/,
-  text: /^\s(.+)/
+  tag: /^([a-z0-9\-]+)/i,
+  id: /^#([a-z0-9\-]+)/i,
+  className: /^\.([a-z0-9\-]+)/i,
+  attr: /^\[([a-z\-0-9]+)(?:="([^"]+)")?\]/i,
+  text: /^\s(.*)/
 };
 
 var doc = void 0;
@@ -48,8 +161,8 @@ domator.toString = function toString(node) {
 };
 
 domator.create = function create() {
-  var name = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
-  var attrs = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+  var attrs = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
   name = parseName(name);['class', 'className'].forEach(function (key) {
     if (typeof name[key] === 'string') name[key] = [name[key]];
@@ -61,7 +174,7 @@ domator.create = function create() {
   var el = doc.createElement(attrs.tag || 'div');
   delete attrs.tag;
 
-  if (attrs.text) {
+  if ('text' in attrs) {
     el.textContent = attrs.text;
     delete attrs.text;
   }
@@ -72,7 +185,7 @@ domator.create = function create() {
 };
 
 function setAttributes(el) {
-  var attrs = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+  var attrs = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
   attrs['class'] = (attrs['class'] || []).concat(attrs.className).join(' ');
   if (attrs.className) delete attrs.className;
